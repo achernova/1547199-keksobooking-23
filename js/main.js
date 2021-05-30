3eab0cd8d5406bae5402775393b6086f6377433c
Anastas <achernova1551@gmail.com>
2021-05-30 22:21:18 +0300

    выполняет задание 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 function getRandomNumber (min, max) {
-  if (min >= 0 && max >= 0) {
+  if (min > max || min === max) {
+    return undefined;
+  }
+
+  if (min >= 0 && max >= 0){
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
@@ -7,8 +11,11 @@ getRandomNumber(30, 100);
 
 function getRandomNumeral (min, max, fixed) {
   const randNumber = min + Math.random() * (max - min);
+  if (min > max || min === max) {
+    return undefined;
+  }
   if (min >= 0 && max >= 0) {
     return randNumber.toFixed(fixed);
   }
 }
-getRandomNumeral(0, 10, 3);
+getRandomNumeral(5, 10, 3);
